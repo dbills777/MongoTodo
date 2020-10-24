49a149809ea2e7769a5fa976ef987791a9707414
Derrick Bills <dbills777@gmail.com>
2020-10-24 17:47:29 -0600

    x

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.delete('/todo/:id', (req, res) => {
 app.delete('/delete', (req, res) => {
   const query = { complete: true };
 
-  todos
+  Todo
     .deleteMany(query)
     .then((result) => console.log(`Deleted ${result.deletedCount} item(s).`))
     .catch((err) => console.error(`Delete failed with error: ${err}`));
