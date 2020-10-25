5b49f974698da4ebb5c0e0874ebd289ed591622b
Derrick Bills <dbills777@gmail.com>
2020-10-24 18:11:24 -0600

    s

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+const e = require("express");
+
 const url = 'https://calm-shelf-89866.herokuapp.com';
 // const todoURL = 'http://localhost:3000';
 
@@ -26,6 +28,7 @@ const reset = document.querySelector('#reset');
 const form = document.querySelector('#myForm');
 todoInput.focus();
 reset.addEventListener('click',()=>{
+  e.preventDefault()
   async function deleteTodos() {
     const response = await fetch(
       `https://calm-shelf-89866.herokuapp.com/delete`,
